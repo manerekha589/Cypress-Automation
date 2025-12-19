438071d6f01e241ccf583269994f0d35838f77e0
Rekha Mane <rmane@poweredbyprisma.com>
2025-12-19 18:26:16 +0530

    Added Cypress automation test

---
+++ cypress/support/commands.js
@@ -0,0 +1,40 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  cy.visit('https://staging-client.dokshop.com')
+
+  cy.get('#cookieConsent button.small', { timeout: 10000 })
+    .should('be.visible')
+    .click({ force: true });
+
+  cy.get('#AccessCode').type('Rymes')
+  cy.get('#Username').type('DevUser')
+  cy.get('#Password').type('prismaUX24')
+  cy.get("button[type='submit']").click()
+
+  cy.contains('Welcome', { timeout: 20000 }).should('be.visible')
+})
